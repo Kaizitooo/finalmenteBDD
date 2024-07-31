4e7c3654c16addb212c3e002709bce89e98ba1e1
CARLOS EDUARDO TEIXEIRA GUIA <021.898687@senaicimatec.edu.br>
2024-07-31 17:53:00 -0300

    conserto

--- app.js
+++ app.js
@@ -1,43 +1,89 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
 const mysql = require('mysql');
+
 const path = require('path');
 
+ 
+
 const app = express();
+
 const port = 3001;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+ 
+
+// Configuração do body-parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+ 
+
+// Configuração do banco de dados
 
 const db = mysql.createConnection({
+
     host: 'localhost',
-    user: 'root',
-    password: 'cimatec',
-    database: 'leads'
+
+    user: 'root', // Substitua pelo seu usuário do MySQL
+
+    password: 'cimatec', // Substitua pela sua senha do MySQL
+
+    database: 'leads', //nome de  seu banco de dados
+
 });
+
+ 
 
 db.connect((err) => {
-    if (err){
+
+    if (err) {
+
         throw err;
+
     }
-    console.log('Conectando ao banco de dados MySQL');
+
+    console.log('Conectado ao banco de dados MySQL.');
+
 });
 
+// Servir o arquivo HTML
+
 app.get('/', (req, res) => {
+
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
-  
-  app.post('/submit', (req, res) => {
+
+});
+
+ 
+
+// Rota para lidar com o formulário de envio
+
+app.post('/submit', (req, res) => {
+
     const { nome, email, telefone } = req.body;
+
     const query = 'INSERT INTO email (nome, email, telefone) VALUES (?, ?, ?)';
-  
+
     db.query(query, [nome, email, telefone], (err, result) => {
-      if (err) {
-        throw err;
-      }
-      res.send('Dados inseridos com sucesso!');
+
+        if (err) {
+
+            throw err;
+
+        }
+
+        res.send('Dados inseridos com sucesso!');
+
     });
-  });
-  
-  app.listen(port, () => {
+
+});
+
+ 
+
+app.listen(port, () => {
+
     console.log('Servidor rodando na porta ${port}');
-  });
+
+});
